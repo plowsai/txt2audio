4d6fb944cfb126e7db7c338a5f8e45b2dd5805da
Jordan Plows <jordan@lightapi.com>
2023-07-18 16:56:22 -0700

    chore: commit save point

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import pyaudio
 
 import assemblyai as aai
 
+# aai.settings.api_key = "1dbc60da72694918aec13a3a6140dbd2"
 aai.settings.api_key = "1dbc60da72694918aec13a3a6140dbd2"
 
 transcriber = aai.Transcriber()
