fb55220a818e06d6bf41edbb238b93887cc0781a
Jordan Plows <jordan@lightapi.com>
2023-07-18 16:54:54 -0700

    chore: commit save point

---
+++ index.py
@@ -0,0 +1,11 @@
+import pyaudio
+
+import assemblyai as aai
+
+aai.settings.api_key = "1dbc60da72694918aec13a3a6140dbd2"
+
+transcriber = aai.Transcriber()
+
+transcript = transcriber.transcribe("./my-local-audio-file.wav")
+
+print(transcript.text)
